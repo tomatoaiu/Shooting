fb008b086d8d5557b70dc10a5d0eefe6faaa9937
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2017-10-03 21:18:07 +0900

    背景スクロール機能追加

---
+++ Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public float y;
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, -speed, 0);
+		if (transform.position.y < -y ) {
+			transform.position = new Vector3 (0, y, 0);
+		}
+	}
+}
